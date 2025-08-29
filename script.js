9047d8f498805deb804207107b73e0270041f8a0
nvamshi12 <nvamshidhar12@gmail.com>
2025-08-29 00:28:31 -0700

    Updated index, script.js to load '.png' files instead of 'gif' files

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const label = document.getElementById("cursor-label");
-document
-  .querySelector(".parent-container")
-  .addEventListener("mousemove", (e) => {
-    if (e.target.tagName === "IMG") {
-      // if the target is an image, show the label
-      // move the label to the mouse position
-      label.style.left = e.clientX + "px";
-      label.style.top = e.clientY + "px";
-      label.style.cursor = "pointer";
-      label.textContent = `Open ${e.target.alt}!`;
-    } else {
-      // if the target is not an image, hide the label
-      label.style.left = "-9999px";
-      label.style.top = "-9999px";
-    }
-  });
-
 document.querySelector(".parent-container").addEventListener("click", (e) => {
   // if the target is an image, open the link
   if (e.target.tagName === "IMG") {
@@ -26,9 +8,32 @@ document.querySelector(".parent-container").addEventListener("click", (e) => {
   }
 });
 
+// Display gif files after they have loaded.
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("img").forEach((img) => {
     console.log(img.alt);
-    img.src = `./assets/${img.alt}.gif`;
+    console.log(img.src);
+    img.src = `./assets/${img.alt}.png`;
   });
 });
+
+// Display info text upon hovering over each image - Eg: "Open Mark my spot"
+
+// const label = document.getElementById("cursor-label");
+
+// document
+//   .querySelector(".parent-container")
+//   .addEventListener("mousemove", (e) => {
+//     if (e.target.tagName === "IMG") {
+//       // if the target is an image, show the label
+//       // move the label to the mouse position
+//       label.style.left = e.clientX + "px";
+//       label.style.top = e.clientY + "px";
+//       label.style.cursor = "pointer";
+//       label.textContent = `Open ${e.target.alt}!`;
+//     } else {
+//       // if the target is not an image, hide the label
+//       label.style.left = "-9999px";
+//       label.style.top = "-9999px";
+//     }
+//   });
